7ca9229dd133dda5e1d0a285cfbea2774f3cdf48
valdez2020 <valdez.ja2019@gmail.com>
2022-09-11 13:06:09 -0300

    Incorporación de estilos de tarjeta para foto, centrado de texto, se decidió la fuente y el tamaño de la misma, se quitó el link de script del html por el momento

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+//Botón para volver al tope de la página
 let arrow = document.querySelector('.to-up');
 
 arrow.addEventListener("click",() => {
@@ -6,3 +7,24 @@ arrow.addEventListener("click",() => {
         scrollTop: 0
     },1000);
 })
+
+
+//Intervalos en animación de presentación
+
+
+let parrafo_2 = document.getElementById('p-2');
+let parrafo_3 = document.getElementById('p-3');
+let titulo = document.getElementById('title');
+
+let clase_1 = "flicker-in-1";
+let clase_2 = "flicker-out-1";
+let clase_3 = "text-focus-in";
+
+
+const show = () =>{
+    let parrafo_1 = document.getElementById('p-1');
+        parrafo_1.classList.remove(clase_1);
+}
+
+
+window.setTimeout(show,4000);
